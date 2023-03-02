c0260e27dd91c08d5b0520d5fc5b715a1cb3a932
Alex Dzyuba <alexeydzyuba27@gmail.com>
2023-03-02 10:11:50 +0100

    Add SCSS support

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
-
 import {Header} from './Home/Header';
 import {PictureUnderHeader} from './Home/PictureUnderHeader';
 import {Main} from './Home/Main/Main';
 import {Footer} from './Home/Footer';
 
-
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
 
 import {FirstSezCont} from './AllSeazons/FirstSez/FirsSezCont';
 import {TwoSezCont} from './AllSeazons/TwoSez/TwoSezCont';
 import {ThreeSezCont} from './AllSeazons/ThreeSez/ThreeSezCont';
-
-
- 
 
 function App() {
   return (

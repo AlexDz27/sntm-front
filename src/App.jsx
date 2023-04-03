da88f0c79f8ece9fab672bcd3a07e5fe4571df7a
Alex Dzyuba <alexeydzyuba27@gmail.com>
2023-04-03 16:50:52 +0200

    e

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import { Home } from './Home'
 
 function App() {
+  // Fix for React Router's behavior that browser viewport remains in same place after going to another page
+  const { pathname } = useLocation()
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
   return (
     <div className="app" id="app">
       <Home />
